05a9c1ba142097a04b3e4721eaab79632608eb6b
Dozawow <dozza.2u8@gmail.com>
2026-06-30 17:09:36 +0500

    Add Kui Nameplates profile, WeakAura and installation guide
    - Added Kui_Nameplates profile
    - Added Kui_Nameplates_Auras profile
    - Added WeakAura
    - Added README with installation guide

---
+++ Зайти перед установкой/Profile/Kui_Nameplates.lua
@@ -0,0 +1,194 @@
+
+KuiNameplatesGDB = {
+	["namespaces"] = {
+		["Castbar"] = {
+			["profiles"] = {
+				["Dozawow - Neverest x3 - 3.3.5a+"] = {
+					["display"] = {
+						["casttime"] = true,
+						["cbheight"] = 10,
+						["barcolour"] = {
+							0.3803921568627451, -- [1]
+							0.9921568627450981, -- [2]
+							0, -- [3]
+						},
+					},
+				},
+			},
+		},
+		["LibDualSpec-1.0"] = {
+			["char"] = {
+				["Dozawow - Neverest x3 - 3.3.5a+"] = {
+					["profile"] = "Dozawow - Neverest x3 - 3.3.5a+",
+					["specGroup"] = 1,
+					["enabled"] = false,
+				},
+			},
+		},
+		["NameOnly"] = {
+			["profiles"] = {
+				["Dozawow - Neverest x3 - 3.3.5a+"] = {
+					["display"] = {
+						["fontsizetrivial"] = 8,
+						["fontsizeplayer"] = 17,
+						["fontsize"] = 17,
+						["ondamaged"] = true,
+					},
+					["colours"] = {
+						["friendly"] = {
+							0, -- [1]
+							nil, -- [2]
+							0.08627450980392157, -- [3]
+							1, -- [4]
+						},
+					},
+				},
+			},
+		},
+		["ClassColours"] = {
+		},
+		["LowHealthColours"] = {
+			["profiles"] = {
+				["Dozawow - Neverest x3 - 3.3.5a+"] = {
+					["colour"] = {
+						nil, -- [1]
+						0.9921568627450981, -- [2]
+						0.9529411764705882, -- [3]
+						1, -- [4]
+					},
+				},
+			},
+		},
+		["CastWarnings"] = {
+		},
+		["Auras"] = {
+			["profiles"] = {
+				["Dozawow - Neverest x3 - 3.3.5a+"] = {
+					["showtrivial"] = true,
+				},
+			},
+		},
+		["ComboPoints"] = {
+		},
+		["TankMode"] = {
+			["profiles"] = {
+				["Dozawow - Neverest x3 - 3.3.5a+"] = {
+					["brackets"] = {
+						["scale"] = 1.003252625465393,
+						["enable_brackets"] = false,
+					},
+					["tankmode"] = {
+						["barcolour"] = {
+							0.1411764705882353, -- [1]
+							1, -- [2]
+							0, -- [3]
+							1, -- [4]
+						},
+						["glowcolour"] = {
+							[3] = 0.007843137254901961,
+						},
+					},
+				},
+			},
+		},
+	},
+	["profileKeys"] = {
+		["Dozawow - Neverest x3 - 3.3.5a+"] = "Dozawow - Neverest x3 - 3.3.5a+",
+	},
+	["profiles"] = {
+		["Dozawow - Neverest x3 - 3.3.5a+"] = {
+			["general"] = {
+				["targetglow"] = false,
+				["twidth"] = 73,
+				["glowshadow"] = false,
+				["raidicon_side"] = 2,
+				["thheight"] = 10,
+				["bartexture"] = "Solid",
+				["targetarrows"] = true,
+				["width"] = 170,
+				["hheight"] = 23,
+				["targetglowcolour"] = {
+					1, -- [1]
+					1, -- [2]
+				},
+			},
+			["healthoffsetx"] = 0,
+			["text"] = {
+				["levelanchorpoint"] = "RIGHT",
+				["healthoffsety"] = -8.5,
+				["nameoffsety"] = 4,
+				["leveloffsetx"] = -0.5,
+				["level"] = true,
+				["leveloffsety"] = 20,
+				["healthanchorpoint"] = "TOP",
+			},
+			["hp"] = {
+				["text"] = {
+					["hp_hostile_max"] = 1,
+					["hp_hostile_low"] = 1,
+				},
+				["reactioncolours"] = {
+					["neutralcol"] = {
+						nil, -- [1]
+						nil, -- [2]
+						nil, -- [3]
+						1, -- [4]
+					},
+					["friendlycol"] = {
+						0.07843137254901961, -- [1]
+						0.9921568627450981, -- [2]
+						0, -- [3]
+						1, -- [4]
+					},
+					["playercol"] = {
+						0.3686274509803922, -- [1]
+						0.6901960784313725, -- [2]
+						1, -- [3]
+						1, -- [4]
+					},
+					["tappedcol"] = {
+						0.6705882352941176, -- [1]
+						0.6705882352941176, -- [2]
+						0.6705882352941176, -- [3]
+						1, -- [4]
+					},
+					["hatedcol"] = {
+						1, -- [1]
+						0, -- [2]
+						0.1254901960784314, -- [3]
+						1, -- [4]
+					},
+				},
+			},
+			["nameanchorpoint"] = "TOP",
+			["levelanchorpoint"] = "RIGHT",
+			["nameoffsety"] = 4,
+			["healthoffsety"] = -8.5,
+			["fonts"] = {
+				["options"] = {
+					["fontscale"] = 1.1,
+					["font"] = "Expressway",
+					["noalpha"] = true,
+				},
+				["sizes"] = {
+					["small"] = 2,
+					["health"] = 20,
+					["spellname"] = 12,
+					["level"] = 10,
+					["name"] = 16,
+					["large"] = 5,
+				},
+			},
+			["fade"] = {
+				["rules"] = {
+					["avoidhostilehp"] = true,
+					["avoidcast"] = true,
+				},
+				["fadedalpha"] = 0.2599999904632568,
+			},
+			["leveloffsetx"] = -0.5,
+			["leveloffsety"] = 20,
+			["healthanchorpoint"] = "TOP",
+		},
+	},
+}
---
+++ Зайти перед установкой/Profile/Kui_Nameplates_Auras.lua
@@ -0,0 +1,24 @@
+
+KuiSpellListCustom = {
+	["Classes"] = {
+		["SHAMAN"] = {
+			[49231] = true,
+		},
+	},
+	["Ignore"] = {
+		["DEATHKNIGHT"] = {
+			[57330] = true,
+			[49222] = true,
+		},
+		["DRUID"] = {
+		},
+		["PALADIN"] = {
+			[19740] = true,
+			[20217] = true,
+			[53563] = true,
+			[53601] = true,
+			[25898] = true,
+			[48932] = true,
+		},
+	},
+}
